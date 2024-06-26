ca787a751135e74d3a442328e095feb7d01f5b39
Kauandlr <kauanlara123@gmail.com>
2024-06-26 14:34:17 -0300

    Primeiro Commit

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        drawer: const Drawer(),
+        body: Center(
+          child: Text('Home')),
+        floatingActionButton: FloatingActionButton(onPressed: ()  {},),
+      )
+        
+    );
+  }
+}
